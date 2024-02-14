f91ff367c849dae215899133586ceec002ea1f8d
Rupal Sharma <rupal.sharma@clouddrove.com>
2024-02-14 16:23:38 +0530

    fix:change in count condition

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "resource_group_id" {
-  value       = azurerm_resource_group.default[0].id
+  value       = azurerm_resource_group.default[*].id
   description = "The ID of the Resource Group."
 }
 
 output "resource_group_name" {
-  value       = azurerm_resource_group.default[0].name
+  value       = azurerm_resource_group.default[*].name
   description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
 }
 
 output "resource_group_location" {
-  value       = azurerm_resource_group.default[0].location
+  value       = azurerm_resource_group.default[*].location
   description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
 }
 
