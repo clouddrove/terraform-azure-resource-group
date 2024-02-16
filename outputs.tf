2a55ff849b48fe938f1d8eea7c6f2e3a91ce6d06
Rupal Sharma <152851038+Rupalgw@users.noreply.github.com>
2024-02-16 13:49:53 +0530

    fix: change in count condition (#19)
    * fix: change in count condition
    
    * fix:change in count condition with null

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "resource_group_id" {
-  value       = azurerm_resource_group.default[0].id
+  value       = try(azurerm_resource_group.default[0].id, null)
   description = "The ID of the Resource Group."
 }
 
 output "resource_group_name" {
-  value       = azurerm_resource_group.default[0].name
+  value       = try(azurerm_resource_group.default[0].name, null)
   description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
 }
 
 output "resource_group_location" {
-  value       = azurerm_resource_group.default[0].location
+  value       = try(azurerm_resource_group.default[0].location, null)
   description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
 }
 
 output "tags" {
-  value       = module.labels.tags
+  value       = try(module.labels.tags, null)
   description = "A mapping of tags which should be assigned to the Resource Group."
 }
